9e10a146e057938be4c45d6f337b41d1ba0f796c
siemiigor-creator <siemiigor@gmail.com>
2026-01-05 02:37:20 +0100

    Implement ItemBuilder for creating items

---
+++ ItemBuilder.java
@@ -0,0 +1,27 @@
+package pl.stormanarchia.utils;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
+
+public class ItemBuilder {
+
+    public static ItemStack of(Material material, String name) {
+        ItemStack item = new ItemStack(material);
+        ItemMeta meta = item.getItemMeta();
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        return item;
+    }
+
+    public static ItemStack head(Player player, String name) {
+        ItemStack item = new ItemStack(Material.PLAYER_HEAD);
+        SkullMeta meta = (SkullMeta) item.getItemMeta();
+        meta.setOwningPlayer(player);
+        meta.setDisplayName(name);
+        item.setItemMeta(meta);
+        return item;
+    }
+}
